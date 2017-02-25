3f09fad869587dad4c516f3e12ca1abff52d9d8a
disablerBA <alex_andr.net@mail.ru>
2017-02-25 13:02:22 +0400

    ДЗ-6, Все, кроме задания со звездочкой.

--- src/index.js
+++ src/index.js
@@ -1,156 +1,43 @@
-/* ДЗ 3 - объекты и массивы */
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
 
-/*
- Задача 1:
- Напишите аналог встроенного метода forEach для работы с массивами
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
  */
-function forEach(array, fn) {
-    for (var i = 0; i < array.length; i++) {
-        fn(array[i], i, array);
-    }
-}
-
-/*
- Задача 2:
- Напишите аналог встроенного метода map для работы с массивами
- */
-function map(array, fn) {
-    var result = [];
-
-    for (var i = 0; i < array.length; i++) {
-        result.push(fn(array[i], i , array));
-    }
-
-    return result;
-}
-
-/*
- Задача 3:
- Напишите аналог встроенного метода reduce для работы с массивами
- */
-function reduce(array, fn, initial) {
-    var result;
-    var initialIndex;
-
-    if (initial !== undefined) {
-        result = initial;
-        initialIndex = 0;
-    } else {
-        result = array[0];
-        initialIndex = 1;
-    }
-
-    for (var i = initialIndex; i < array.length; i++) {
-        result = fn(result, array[i], i, array);
-    }
-
-    return result;
-}
-
-/*
- Задача 4:
- Функция принимает объект и имя свойства, которое необходиом удалить из объекта
- Функция должна удалить указанное свойство из указанного объекта
- */
-function deleteProperty(obj, prop) {
-    delete obj[prop];
-}
-
-/*
- Задача 5:
- Функция принимает объект и имя свойства и возвращает true или false
- Функция должна проверить существует ли укзаанное свойство в указанном объекте
- */
-function hasProperty(obj, prop) {
-    return obj.hasOwnProperty(prop);
-}
-
-/*
- Задача 6:
- Функция должна получить все перечисляемые свойства объекта и вернуть их в виде массива
- */
-function getEnumProps(obj) {
-    var result = [];
-
-    for (var prop in obj) {
-        result.push(prop);
-    }
-
-    return result;
-}
-
-/*
- Задача 7:
- Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистра и вернуть в виде массива
- */
-function upperProps(obj) {
-    var result = getEnumProps(obj);
-
-    for (var i = 0; i < result.length; i++) {
-        result.splice(i, 1, result[i].toUpperCase());
-    }
-
-    return result;
-}
-
-/*
- Задача 8 *:
- Напишите аналог встроенного метода slice для работы с массивами
- */
-function slice(array, from, to) {
-    var result = [];
-
-    if (from === undefined) {
-        from = 0;
-        to = array.length;
-    }
-
-    if (to === undefined) {
-        to = array.length;
-    }
-
-    if (from < 0) {
-        from += array.length;
-
-        if (from < 0) {
-            from = 0;
-        }
-    }
-
-    if (to < 0 ) {
-        to += array.length;
-    }
-
-    if (to < from) {
-        return result;
-    }
-
-    result = Array.from(array);
-    return result.splice(from, to - from);
-}
-
-/*
- Задача 9 *:
- Функция принимает объект и должна вернуть Proxy для этого объекта
- Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
- */
-function createProxy(obj) {
-    return new Proxy(obj, {
-        set(target, prop, value) {
-            target[prop] = value * value;
-            return true;
-        }
+function delayPromise(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), seconds * 1000);
     });
 }
 
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+function loadAndSortTowns() {
+    let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+
+    return new Promise((resolve) => {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        xhr.send();
+
+        xhr.addEventListener('load', () => {
+            var sortTowns = Array.from(xhr.response).sort((town1, town2) => town1.name.localeCompare(town2.name));
+            resolve(sortTowns);
+        });
+    });
+
+}
+
 export {
-    forEach,
-    map,
-    reduce,
-    deleteProperty,
-    hasProperty,
-    getEnumProps,
-    upperProps,
-    slice,
-    createProxy
+    delayPromise,
+    loadAndSortTowns
 };
