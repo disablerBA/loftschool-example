19b80942daea984673d22e0af7a79dbe41fbd596
disablerBA <alex_andr.net@mail.ru>
2017-03-31 22:14:08 +0400

    ДЗ-9. Окно для комментариев. Возможность оставлять комментарии. Группировка меток. Окно группы меток.

--- src/index.js
+++ src/index.js
@@ -1,205 +1,137 @@
-let allFriendsListTemplateFn = require('../all-friends-list.hbs');
-let selectedFriendsListTemplateFn = require('../selected-friends-list.hbs');
+let commentsPopupTemplateFn = require('../comments-popup.hbs');
+let commentsTemplateFn = require('../comments.hbs');
+let balloonBodyTemplateFn = require('../balloonBody.hbs');
 
-let friendSelector = document.querySelector('#friend-selector');
-let allFriendsDiv = document.querySelector('#all-friends-list');
-let selectedFriendsDiv = document.querySelector('#selected-friends-list');
-let saveButton = document.querySelector('#saveButton');
+var myMap;
+var clusterer;
 
-let allFriends = [];
-let selectedFriends = [];
+ymaps.ready(init);
 
-let friendsPromise = getFriends();
-
-login().then(() => {
-    return friendsPromise;
-}).then(result => {
-    if (isSavedFriendsToLocalStorage()) {
-        actualizeFriends(result);
-    } else {
-        initializeFriends(result);
-    }
-
-    renderFriends(allFriends, selectedFriends);
-}).catch((e) => {
-    alert(`Ошибка ${e}`)
-});
-
-function login() {
-    return new Promise((resolve, reject) => {
-        VK.init({
-            apiId: 5561322
-        });
-
-        VK.Auth.login(function(result) {
-            if (result.status == 'connected') {
-                resolve();
-            } else {
-                reject();
-            }
-        });
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [55.751574, 37.573856],
+        zoom: 9
+    }, {
+        searchControlProvider: 'yandex#search',
     });
-}
 
-function getFriends() {
-    return new Promise((resolve, reject) => {
-        VK.api('friends.get', { v: 5.62, fields: 'photo_50' }, function(result) {
-            if (result.error) {
-                reject();
+    clusterer = new ymaps.Clusterer({
+        /**
+         * Через кластеризатор можно указать только стили кластеров,
+         * стили для меток нужно назначать каждой метке отдельно.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/option.presetStorage.xml
+         */
+        preset: 'islands#invertedVioletClusterIcons',
+        /**
+         * Ставим true, если хотим кластеризовать только точки с одинаковыми координатами.
+         */
+        groupByCoordinates: false,
+        /**
+         * Опции кластеров указываем в кластеризаторе с префиксом "cluster".
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ClusterPlacemark.xml
+         */
+        clusterBalloonContentLayout: 'cluster#balloonCarousel',
+        clusterDisableClickZoom: true,
+        clusterHideIconOnBalloonOpen: false,
+        geoObjectHideIconOnBalloonOpen: false
+    });
+
+    myMap.geoObjects.add(clusterer);
+
+    myMap.events.add('click', (e) => {
+        const coords = e.get('coords');
+
+        ymaps.geocode(coords).then((res) => {
+            if (!isOpenPopup()) {
+                const title = res.geoObjects.get(0).properties.get('name');
+                openPopup(title, e.get('offsetX'), e.get('offsetY'), coords);
             } else {
-                resolve(result.response.items);
+                closePopup();
             }
         })
     });
 }
 
-function renderFriends(allFriendsId, selectedFriendsId) {
-    renderAllFriends(allFriendsId);
-    renderSelectedFriends(selectedFriendsId);
-}
+function openPopup(title, offsetX, offsetY, coords) {
+    const commentsPopup = document.createElement('div');
+    commentsPopup.innerHTML = commentsPopupTemplateFn({title: title});
+    commentsPopup.id = 'commentsPopup';
+    commentsPopup.className = 'container';
+    commentsPopup.style.left = `${offsetX}px`;
+    commentsPopup.style.top = `${offsetY}px`;
+    document.body.appendChild(commentsPopup);
 
-function renderAllFriends(friendsToRenderId) {
-    friendsPromise.then( friends => {
-        let friendsToRender = friends.filter(f => friendsToRenderId.indexOf(f.id) !== -1);
-        allFriendsDiv.innerHTML = allFriendsListTemplateFn({friends: friendsToRender});
-    });
-}
+    const closeButton = document.querySelector('.closeButton');
+    closeButton.onclick = function () {
+        closePopup();
+    };
 
-function renderSelectedFriends(friendsToRenderId) {
-    friendsPromise.then( friends => {
-        let friendsToRender = friends.filter(f => friendsToRenderId.indexOf(f.id) !== -1);
-        selectedFriendsDiv.innerHTML = selectedFriendsListTemplateFn({friends: friendsToRender});
-    });
-}
-
-function initializeFriends(friends) {
-    allFriends = friends.map(friend => {
-        return friend.id;
-    });
-
-    selectedFriends = [];
-}
-
-function actualizeFriends(friends) {
-    actualizeAllFriends(friends);
-    actualizeSelectedFriends(friends);
-}
-
-function actualizeAllFriends(friends) {
-    selectedFriends = JSON.parse(localStorage.selectedFriends);
-
-    allFriends = friends.map(f => f.id).filter(id => selectedFriends.indexOf(id) === -1);
-    localStorage.allFriends = JSON.stringify(allFriends);
-}
-
-function actualizeSelectedFriends(friends) {
-    selectedFriends = JSON.parse(localStorage.selectedFriends);
-
-    selectedFriends = selectedFriends.filter(id => friends.map(f => f.id).indexOf(id) !== -1);
-    localStorage.selectedFriends = JSON.stringify(selectedFriends);
-}
-
-function isSavedFriendsToLocalStorage() {
-    return localStorage.allFriends && localStorage.selectedFriends;
-}
-
-function saveFriendsToLocalStorage() {
-    localStorage.allFriends = JSON.stringify(allFriends);
-    localStorage.selectedFriends = JSON.stringify(selectedFriends);
-}
-
-function isMatching(fullName, chunkName) {
-    if (chunkName === "") return false;
-    return fullName.toLowerCase().indexOf(chunkName.trim().toLowerCase()) !== -1;
-}
-
-
-friendSelector.addEventListener('keyup', event => {
-    if (event.target.id === 'allFriendsFilter') {
-        let allFriendsFilter = document.querySelector('#allFriendsFilter');
-        let search = allFriendsFilter.value;
-
-        if (search.length === 0) {
-            renderAllFriends(allFriends);
-            return;
-        }
-
-        filterFriends(allFriends, search).then(filtered => renderAllFriends(filtered));
+    const addCommentButton = commentsPopup.querySelector('#addCommentButton');
+    addCommentButton.onclick = function () {
+        const senderNameInput = commentsPopup.querySelector('#senderNameInput');
+        const placeInput = commentsPopup.querySelector('#placeInput');
+        const messageTextArea = commentsPopup.querySelector('#messageTextArea');
+        const currentTime = new Date();
+        const comment = {
+            title: title,
+            senderName: senderNameInput.value,
+            time: currentTime,
+            place: placeInput.value,
+            message: messageTextArea.value
+        };
+        addCommentToLocalStorage(coords, comment);
+        updateComments(coords);
+        addMarkerOnMap(coords);
+        senderNameInput.value = '';
+        placeInput.value = '';
+        messageTextArea.value = '';
     }
-
-    if (event.target.id === 'selectedFriendsFilter') {
-        let selectedFriendsFilter = document.querySelector('#selectedFriendsFilter');
-        let search = selectedFriendsFilter.value;
-
-        if (search.length === 0) {
-            renderSelectedFriends(selectedFriends);
-            return;
-        }
-
-        filterFriends(selectedFriends, search).then(filtered => renderSelectedFriends(filtered));
-    }
-});
-
-function filterFriends(friends, chunkName) {
-    return friendsPromise.then(result => {
-
-        let filtered = result.filter(f => friends.indexOf(f.id) !== -1)
-                             .filter(f => isMatching(f.first_name.concat(' ', f.last_name), chunkName));
-        return filtered.map(f => f.id);
-    });
 }
 
-friendSelector.addEventListener('click', event => {
-    friendsPromise.then(() => {
-        if (event.target.dataset.role == 'toSelected') {
-            toSelected(event.target.dataset.id);
-            renderFriends(allFriends, selectedFriends);
-        }
+function closePopup() {
+    const popup = document.querySelector('#commentsPopup');
+    document.body.removeChild(popup);
+}
 
-        if (event.target.dataset.role == 'toAll') {
-            toAll(event.target.dataset.id);
-            renderFriends(allFriends, selectedFriends);
-        }
+function isOpenPopup() {
+    return !!document.querySelector('.closeButton');
+}
 
-        if (event.target.id == 'saveButton') {
-            saveFriendsToLocalStorage();
+function addCommentToLocalStorage(key, comment) {
+    const comments = window.localStorage.getItem(key) ? JSON.parse(window.localStorage.getItem(key)) : [];
+    comments.push(comment);
+    window.localStorage.setItem(key, JSON.stringify(comments));
+}
+
+function addMarkerOnMap(coords) {
+    const comments = JSON.parse(window.localStorage.getItem(coords));
+    const lastComment = comments[comments.length - 1];
+
+    const content = `<a href="#">${lastComment.title}</a><br>${lastComment.message}`;
+    // const content = balloonBodyTemplateFn(lastComment);
+
+    const myPlacemark = new ymaps.Placemark(coords, {
+        balloonContentHeader: lastComment.place,
+        balloonContentBody: content,
+        balloonContentFooter: lastComment.time,
+    });
+
+    myPlacemark.events.add('click', function (e) {
+        if (!isOpenPopup()) {
+            openPopup(lastComment.title, e.get('offsetX'), e.get('offsetY'), coords);
+            updateComments(coords);
         }
     });
-});
 
-function toSelected(id) {
-    id = +id;
-    selectedFriends.push(id);
-    allFriends = allFriends.filter(i => i !== id);
+    myMap.geoObjects.add(myPlacemark);
+    clusterer.add(myPlacemark);
 }
 
-function toAll(id) {
-    id = +id;
-    allFriends.push(id);
-    selectedFriends = selectedFriends.filter(i => i !== id);
+function updateComments(key) {
+    const comments = window.localStorage.getItem(key) ? JSON.parse(window.localStorage.getItem(key)) : [];
+
+    const popup = document.querySelector('#commentsPopup');
+    const commentsPanel = popup.querySelector('#comments');
+
+    commentsPanel.innerHTML = commentsTemplateFn({comments});
 }
-
-document.addEventListener('drop', e => {
-    e.preventDefault();
-    var id = e.dataTransfer.getData('text');
-
-    if (e.target.id === 'selected-friends-list' || e.target.parentNode.closest('#selected-friends-list')) {
-        toSelected(id);
-        renderFriends(allFriends, selectedFriends);
-    }
-    if (e.target.id === 'all-friends-list' || e.target.parentNode.closest('#all-friends-list')) {
-        toAll(id);
-        renderFriends(allFriends, selectedFriends);
-    }
-});
-
-document.addEventListener('dragover', e => {
-    if (e.target.id === 'selected-friends-list' || e.target.id === 'all-friends-list' ||
-        e.target.parentNode.closest('#selected-friends-list') || e.target.parentNode.closest('#all-friends-list')) {
-        e.preventDefault();
-    }
-});
-
-document.addEventListener('dragstart', e => {
-    e.dataTransfer.setData('text', e.target.dataset.id);
-});
